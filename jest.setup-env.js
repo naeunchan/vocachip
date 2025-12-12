98745c4c3571ada61cc732966a396c92ebd0aa38
naeunchan <eunchan0492@naver.com>
2025-12-12 16:47:09 +0900

    fix: add files

--- jest.setup-env.js
+++ jest.setup-env.js
@@ -1,3 +1,9 @@
 if (typeof globalThis.__ExpoImportMetaRegistry !== "function") {
     globalThis.__ExpoImportMetaRegistry = () => ({});
 }
+
+// Ensure env module resolution does not break tests
+jest.mock("react-native-dotenv", () => ({
+    EXPO_PUBLIC_OPENAI_PROXY_URL: "",
+    EXPO_PUBLIC_OPENAI_PROXY_KEY: "",
+}));

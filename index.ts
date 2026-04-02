f1f9178421f791d90f06e6d018052c4e7d282001
naeunchan <eunchan0492@naver.com>
2026-04-02 23:44:54 +0900

    Use Apps in Toss entry for dev runtime

--- index.ts
+++ index.ts
@@ -2,9 +2,9 @@ import "react-native-gesture-handler";
 
 import { registerRootComponent } from "expo";
 
-import App from "./App";
+import AppsInTossRoot from "./src/_app";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(AppsInTossRoot);
